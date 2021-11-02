00d7b53f69a5cf6e1619d001a163f587d45dbe5b
MrFatkhulloh <eternallfame@gmail.com>
2021-11-02 20:47:05 +0500

    add comma and spaces

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
 alert("Sayt hazil tarzida tuzilgan. Davom etish uchun 'ok' tugmasini bosing.")
 
 var userName = prompt("Imingiz?");
-console.log(userName, typeof userName);
+console.log(userName , typeof userName);
 alert("Xush " + "kelibsiz " +  userName + "!" + " Davom etamizmi?");
 
 var userFirstName = prompt("Familyangiz?");
-console.log(userFirstName,, typeof userFirstName);
+console.log(userFirstName , typeof userFirstName);
 alert("Ism-Familyangiz juda ham chiroyli ekan " + userName + " " +  userFirstName + "!" + " Yana davom etamizmi?");
 
 var userAge = prompt("Yoshingiz?");
-console.log(userAge, typeof userAge);
+console.log(userAge , typeof userAge);
 alert("Biz aynan " + userAge + " " + "yoshdagi insonlarni saytimizga tashrif buyurishlarini xohlagan edik!" + " Iltimos davom eting!");
 
 var userNationality = prompt("Millatingiz?");
-console.log(userNationality, typeof userNationality);
+console.log(userNationality , typeof userNationality);
 alert("Tabriklaymiz! Hurmatli " + userName + "! " + "Bizning jamoa " + userAge + " yoshdagi " + userNationality + " foydalanuvchilarga orzularidagi avtomobillarini xarid qilishda davom etmoqda. Davom etish uchun 'ok' tugmasini bosing.");
 
 var userCarName = prompt("Orzuingizdagi mashinangiz?");
-console.log(userCarName, typeof userCarName);
+console.log(userCarName , typeof userCarName);
 alert("Didingizga qoyil!" + " " + "savollar zeriktirmayotgan bo'lsa, davom etaveramiz a?");
 
 var userCarColor = prompt("Mashina qaysi rangda bo'lishini xohlaysiz?");
-console.log(userCarColor, typeof userCarColor);
+console.log(userCarColor , typeof userCarColor);
 alert("Bizda " + userCarColor + " ranglisi bor." + " Davom etamizmi?");
 
 var userRegion = prompt("Mashinani qaysi viloyatga yetkazib beraylik?");
-console.log(userRegion, typeof userRegion);
+console.log(userRegion , typeof userRegion);
 alert(userRegion + " da filiallarimiz bor. Keyingisiga o'ting.");
 
 var userAvenue = prompt("Qaysi tumanga yetkazish kerak?");
-console.log(userAvenue, typeof userAvenue);
+console.log(userAvenue , typeof userAvenue);
 alert("Tushunarli. Keyingi savolga o'ting.");
 
 var userTime = prompt("yetkazish uchun sizga qaysi vaqt qulay?");
-console.log(userTime, typeof userTime);
+console.log(userTime , typeof userTime);
 alert("Unda kelishdik. " + userTime + " ga yaqin xodimlarimiz siz bilan bog'lanishadi.");
 
 var userGame = prompt("Bo'sh vaqtingizda qanaday o'yinlar o'ynaysiz?");
-console.log(userGame, typeof userGame);
+console.log(userGame , typeof userGame);
 alert("Hurmatli " + userName + " " + userFirstName + "," + " ogohlantirganimizdek hammasi hazil. Biroq sizga maslahat bera olamiz xalos. Orzuingizdagi " + userCarColor + " rangli " + userCarName + " avtomobiliga erisha olmaysiz agar unga loyiq bo'lmasangiz. " + userAge + " yoshda orzuni maqsadga aylantirib, " + userGame + " o'ynash o'rniga " + userCarColor + " rangli " + userCarName + " ni qanday qo'lga kiritish yo'llarini o'ylash va unga intilish kerak. Xarakatni hozirdan boshlang maylimi?!");
 
