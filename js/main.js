85555ef11b5e496bd3415797a80f44979911ed67
MrFatkhulloh <eternallfame@gmail.com>
2021-11-02 20:20:29 +0500

    add js file and codes

---
+++ js/main.js
@@ -0,0 +1,42 @@
+alert("Sayt xazil tarzida tuzilgan. Davom etish uchun 'ok' tugmasini bosing.")
+
+var userName = prompt("Imingiz?");
+console.log(userName);
+alert("Xush " + "kelibsiz " +  userName + "!" + " Davom etamizmi?");
+
+var userFirstName = prompt("Familyangiz?");
+console.log(userFirstName);
+alert("Ism-Familyangiz juda ham chiroyli ekan " + userName + " " +  userFirstName + "!" + " Yana davom etamizmi?");
+
+var userAge = prompt("Yoshingiz?");
+console.log(userAge);
+alert("Biz aynan " + userAge + " " + "yoshdagi insonlarni saytimizga tashrif buyurishlarini xohlagan edik!" + " Iltimos davom eting!");
+
+var userNationality = prompt("Millatingiz?");
+console.log(userNationality);
+alert("Tabriklaymiz! Xurmatli " + userName + "! " + "Bizning jamoa " + userAge + " yoshdagi " + userNationality + " foydalanuvchilarga orzularidagi avtomobillarini xarid qilishda davom etmoqda. Davom etish uchun 'ok' tugmasini bosing.");
+
+var userCarName = prompt("Orzuingizdagi mashinangiz?");
+console.log(userCarName);
+alert("Didingizga qoyil!" + " " + "savollar zeriktirmayotgan bo'lsa, davom etaveramiz a?");
+
+var userCarColor = prompt("Mashina qaysi rangda bo'lishini xohlaysiz?");
+console.log(userCarColor);
+alert("Bizda " + userCarColor + " ranglisi bor." + " Davom etamizmi?");
+
+var userRegion = prompt("Mashinani qaysi viloyatga yetkazib beraylik?");
+console.log(userRegion);
+alert(userRegion + " da filiallarimiz bor. Keyingisiga o'ting.");
+
+var userAvenue = prompt("Qaysi tumanga yetkazish kerak?");
+console.log(userAvenue);
+alert("Tushunarli. Keyingi savolga o'ting.");
+
+var userTime = prompt("yetkazish uchun sizga qaysi vaqt qulay?");
+console.log(userTime);
+alert("Unda kelishdik. " + userTime + " ga yaqin hodimlarimiz siz bilan bog'lanishadi.");
+
+var userGame = prompt("Bo'sh vaqtingizda qanaday o'yinlar o'ynaysiz?");
+console.log(userGame);
+alert("Xurmatli " + userName + " " + userFirstName + "," + " ogohlantirganimizdek hammasi xazil. Biroq sizga maslahat bera olamiz xalos. Orzuingizdagi " + userCarColor + " " + userCarName + " avtomobiliga erisha olmaysiz agar unga loyiq bo'lmasangiz. " + userAge + " yoshda orzuni maqsadga aylantirib, " + userGame + " o'ynash o'rniga " + userCarColor + " " + userCarName + " ni qanday qo'lga kiritish yo'llarini o'ylash va unga intilish kerak. Xarakatni hozirdan boshlang!");
+
